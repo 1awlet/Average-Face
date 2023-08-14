5f204a02178b4007c9ef4037be062539018a934c
Gele <gellehussen@gmail.com>
2023-08-14 23:25:44 +0200

    Storing the images in memory

--- sketch.js
+++ sketch.js
@@ -4,12 +4,18 @@ var numOfImages = 30;
 
 //////////////////////////////////////////////////////////
 function preload() { // preload() runs once
-
+    for (let i = 0; i < numOfImages; i++) {
+        const filename = `./assets/${i}.jpg`;
+      imgs.push([loadImage(filename)]);
+      }
 }
+
+
 //////////////////////////////////////////////////////////
 function setup() {
     createCanvas(100, 100);
     pixelDensity(1);
+
 }
 //////////////////////////////////////////////////////////
 function draw() {
