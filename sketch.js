f55aa2b3953c3f04c6a42f47d9902e2a9e9b2db5
Gele <gellehussen@gmail.com>
2023-08-22 23:44:51 +0200

    Provided comments

--- sketch.js
+++ sketch.js
@@ -50,11 +50,17 @@ function loadIMages (){
     }
 }
 
+
+// This function figures out what a combined image would look like if you mixed the colors of several images together.
+// It makes a new image where each pixel's color is the average of the matching pixels
+// from a group of images.
 function calculateAverageImage (){
 
  avgImg = createImage (currentImg.width, currentImg.height );
 
     avgImg.loadPixels();
+    
+     // Loop through each pixel of the current image
     for (let y = 0; y < currentImg.height; y++) {
         for (let x = 0; x < currentImg.width; x++) {
             let index = (x + y * currentImg.width) * 4;
@@ -64,6 +70,7 @@ function calculateAverageImage (){
             avgImg.pixels[index + 3] = 255; // Set alpha to 255
             let sumR = 0, sumG = 0, sumB = 0, sumA = 0;
 
+            // Loop through each image in the imgs array and accumulate pixel values
             for (let i = 0; i < imgs.length; i++) {
                 
                 sumR += imgs[i].pixels[index];
@@ -71,18 +78,20 @@ function calculateAverageImage (){
                 sumB += imgs[i].pixels[index + 2];
                 sumA += imgs[i].pixels[index +3];
             }
+               // Calculate the average color values 
             let avgR = sumR / imgs.length;
             let avgG = sumG / imgs.length;
             let avgB = sumB / imgs.length;
             let avgA = sumA / imgs.length;
 
-       
+       // Use linear interpolation (lerp) to blend the current image's pixel with the average color
             avgImg.pixels[index] = lerp(currentImg.pixels[index], avgR, amt);
             avgImg.pixels[index + 1] = lerp(currentImg.pixels[index+1], avgG, amt);
             avgImg.pixels[index + 2] = lerp(currentImg.pixels[index+2], avgB, amt);
             avgImg.pixels[index + 3] = lerp(currentImg.pixels[index+3], avgA, amt);;
         }
     }
+      // Update pixel data for the average image
     avgImg.updatePixels();
 }
 
